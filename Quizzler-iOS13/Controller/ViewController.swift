89efed2abf98a4c282e6f5d28f2cba2b0ce0d85a
Corinna Brock <corinna.brock@gmail.com>
2024-05-31 10:51:16 -0400

    refactor into MVC

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    var qb = QuizBrain()
+    
+    var timer = Timer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+        
+    }
+
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        let realAnswer = qb.checkAnswer(userAnswer)
+        
+        if realAnswer {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        
+        qb.nextQuestion()
+    }
+    
+    @objc func updateUI() {
+        questionLabel.text = qb.getQuestionText()
+        progressBar.progress = qb.getProgress()
+        
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+
+    }
+}
+
---
+++ Quizzler-iOS13/Model/QuizBrain.swift
@@ -0,0 +1,54 @@
+//
+//  QuizBrain.swift
+//  Quizzler-iOS13
+//
+//  Created by Corinna Moore on 5/30/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct QuizBrain {
+    let quiz = [
+        Question(q: "In The Little Mermaid, Ariel almost had a dolphin sidekick, instead of Flounder.", a: "True"),
+        Question(q: "Mickey Mouse was almost named Marty.", a: "False"),
+        Question(q: "In Peter Pan, Nana originally was going to go with the kids to Neverland.", a: "True"),
+        Question(q: "In Beauty and the Beast, Belle is the only one in her village that wears blue.", a: "True"),
+        Question(q: "In Alice in Wonderland, the White Rabbit's watch always reads 12:00.", a: "False"),
+        Question(q: "In Sleeping Beauty, Aurora is only onscreen for 18 minutes.", a: "True"),
+        Question(q: "Cinderella was the first-ever Disney princess", a: "False"),
+        Question(q: "In 'Snow White and the Seven Dwarfs,' the Evil Queen recites the line, 'Magic mirror on the wall, who is the fairest one of all?'", a: "True"),
+        Question(q: "The name of Princess Jasmine's tiger is Rajah.", a: "True"),
+        Question(q: "Released in 1991, 'Beauty and the Beast' was the last Disney movie that Walt Disney oversaw.", a: "False"),
+        Question(q: "The three fairies in 'Sleeping Beauty' are known as Flora, Fauna, and Fiona.", a: "False"),
+        Question(q: "The title character of every single Disney film ever produced has had spoken lines.", a: "False")
+    ]
+    
+    var qn = 0
+    
+    func checkAnswer(_ userAnswer: String) -> Bool {
+        if userAnswer == quiz[qn].answer {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func getQuestionText() -> String {
+       return quiz[qn].text
+    }
+    
+    func getProgress() -> Float {
+        return Float(qn + 1) / Float(quiz.count)
+    }
+    
+    mutating func nextQuestion() {
+        let ql = quiz.count-1
+        
+        if qn >= ql {
+            qn = 0
+        } else {
+            qn += 1
+        }
+    }
+}
